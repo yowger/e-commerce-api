50e6353ba54296dca287916f80b68d2944d50b7a
Ver0018 <rogerpantilyowger@gmail.com>
2025-02-23 04:50:01 +0800

    updated docker compose, added env.sample

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,27 @@ import { productRouter } from "@/features/catalog/pres/routes/productRoutes"
 import { container } from "@/shared/di"
 import App from "@/App/App"
 import defaultRoutes from "@/App/defaultRoutes"
+import { Pool } from "pg"
 
 const PORT = process.env.PORT || 3000
 
 const app = container.get(App)
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT),
+})
+
+console.log("nice")
 
 app.configureDefaultMiddleware()
 app.configureSwaggerDocs()
 app.registerRoutes("/api/v1/catalog", productRouter)
 app.registerRoutes("/api", defaultRoutes)
 app.configureNotFoundHandler()
-app.configureErrorHandling() 
+app.configureErrorHandling()
 
 app.start(PORT)
