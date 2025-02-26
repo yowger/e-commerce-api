391f361a686245d539cbba39dbb42fd5682e43d0
Ver0018 <rogerpantilyowger@gmail.com>
2025-02-26 23:18:02 +0800

    fix port

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { notFoundHandler } from "@/lib/http/middleware/notFoundHandler"
 import { SwaggerService } from "@/lib/swagger/SwaggerService"
 
 // const PORT = process.env.PORT || 3000
-const PORT = 4000
+const PORT = 3000
 
 const swaggerService = new SwaggerService()
 const app = new App(swaggerService)
